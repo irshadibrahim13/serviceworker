55736cd4a5e84a8b5515651a73b23a2a6dfe7a80
Irshad <irshadibrahim13@gmail.com>
2019-12-11 11:39:41 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 if('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker
-            .register('/sw_caches_pages.js')
+            .register('sw_caches_pages.js')
             .then(req => console.log('Service Worker: Registered'))
             .catch(err => console.log(`Service Worker: Error: ${err}`));
     })
